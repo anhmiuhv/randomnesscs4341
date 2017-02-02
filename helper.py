a7e2587d1d3cc9be1ecd77da04be81eb8ce45cde
lthoang@wpi.edu <anhmiuhv@yahoo.com.vn>
2017-02-01 22:13:43 -0500

    improve isPrime function

--- helper.py
+++ helper.py
@@ -1,17 +1,18 @@
 from math import sqrt
 import random
 
+prime = {9: False, 8: False, 7: True, 6: False, 5: True, 4: False, 3:True, 2:True}
 def decision(probability):
     return random.random() < probability
 
 def isPrime(num):
     ret = True
-    if num == 0 or num == 1:
+    if num == 0 or num == 1 or num < 0:
         return False
-    for i in range(2, int(sqrt(num)) + 1):
-        if num % i == 0:
-            return False
-    return ret
+#     for i in range(2, int(sqrt(num)) + 1):
+#         if num % i == 0:
+#             return False
+    return prime[num]
 
 neginf = -1000000 # negative infinity
 inf = 1000000   # positive infinity
